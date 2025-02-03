a787515e180804481399ba6b8867227449948ad5
pb1100 <prabhakarbalgeri4@gmail.com>
2025-02-03 12:54:25 +0530

    added fsubnet

--- aws-vpc-files/vpc.tf
+++ aws-vpc-files/vpc.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "vpc" {
     Name = "awsvpc"
   }
 }
+
+# Frontend Subnet
+resource "aws_subnet" "fsubnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "login-fsubnet"
+  }
+}
