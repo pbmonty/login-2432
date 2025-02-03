e89c4304d4f512a302551317e1547c9e5b894a2e
pb1100 <prabhakarbalgeri4@gmail.com>
2025-02-03 11:58:55 +0530

    alllll

---
+++ aws-vpc-files/variable.tf
@@ -0,0 +1,14 @@
+# Variables
+variable access_key {
+
+type = string
+description = "AKIAQ4J5YPJSRCLL77ZB"
+
+}
+
+variable secret_key {
+
+type = string
+description = "RqH0i9dZXHXLJkZjlsCPkG0/sZzRYdJIjbADLgjI"
+
+}
