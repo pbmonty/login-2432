adb75c9d16ce3db7d58ce06843466c75ddb4a0b8
pb1100 <prabhakarbalgeri4@gmail.com>
2025-02-03 12:58:15 +0530

    mm

--- aws-vpc-files/vpc.tf
+++ aws-vpc-files/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "fsubnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "login-fsubnet"
