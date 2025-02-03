2a7a40d7fc400768e3ba03da840fb1702600a8b1
pb1100 <prabhakarbalgeri4@gmail.com>
2025-02-03 13:03:31 +0530

    commit DB subnet

--- aws-vpc-files/vpc.tf
+++ aws-vpc-files/vpc.tf
@@ -27,3 +27,14 @@ resource "aws_subnet" "bsubnet" {
     Name = "login-bsubnet"
   }
 }
+
+#DB Subnet
+resource "aws_subnet" "dbsubnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "login-dbsubnet"
+  }
+}
