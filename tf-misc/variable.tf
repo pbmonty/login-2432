fe441dfe49d741577354d2504af3967d004d1cd8
pb1100 <prabhakarbalgeri4@gmail.com>
2025-02-05 12:44:49 +0530

    added variable

---
+++ tf-misc/variable.tf
@@ -0,0 +1,10 @@
+# Variables
+variable aws_access_key {
+  type        = string
+  description = "Input AWS Access Key"
+}
+
+variable aws_secret_key {
+  type        = string
+  description = "Input AWS Secret Key"
+}
